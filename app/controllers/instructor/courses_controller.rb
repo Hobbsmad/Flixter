928a1d1046ae8f01c0f635393383ca139742d771
Madelyn Hobbs <madelynhobbs@Madelyns-MacBook-Pro.local>
2017-04-25 12:01:36 -0500

    DRY

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,6 +1,7 @@
 class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
   before_action :require_authorized_for_current_course, only: [:show]
+  before_action :current_course, only: [:show]
 
   def new
     @course = Course.new
@@ -17,7 +18,6 @@ class Instructor::CoursesController < ApplicationController
 
 
   def show
-    @course ||= Course.find(params[:id])
     end
 
   private
