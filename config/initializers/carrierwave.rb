acadecd529c147375b9f62bae584fab31a40b976
Madelyn Hobbs <madelynhobbs@Madelyns-MacBook-Pro.local>
2017-05-03 20:37:21 -0500

    videos

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
   config.storage    = :aws
-  config.aws_bucket = ENV["AWS_BUCKET"]
+  config.aws_bucket = "oikawamaddy"
   config.aws_acl    = "public-read"
 
   config.aws_credentials = {
-      access_key_id:     ENV["AKIAI6PPPO4FTXGRR7DQ"],
-      secret_access_key: ENV["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"],
-      region:            ENV["Canada_Central"]
+      access_key_id:     "AKIAI6PPPO4FTXGRR7DQ",
+      secret_access_key: "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",
+      region:            'ca-central-1'
   }
 end
