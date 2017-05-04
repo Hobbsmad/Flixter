7ae4fb84a6628f9ba4ce1c8c92fdb87af4b11b0b
Madelyn Hobbs <madelynhobbs@Madelyns-MacBook-Pro.local>
2017-05-03 22:00:32 -0500

    fixed

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
   config.storage    = :aws
-  config.aws_bucket = "oikawamaddy"
+  config.aws_bucket = ENV['AWS_BUCKET']
   config.aws_acl    = "public-read"
 
   config.aws_credentials = {
-      access_key_id:     "AKIAI6PPPO4FTXGRR7DQ",
-      secret_access_key: "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",
-      region:            'ca-central-1'
+      access_key_id:     ENV['AWS_ACCESS_KEY'],
+      secret_access_key: ENV['AWS_SECRET_KEY'],
+      region:            ENV['AWS_REGION']
   }
 end
